c2c34f0d30a783153e1c4575787810b77bbabf22
RobARC <rrondonc@gmail.com>
2021-03-10 11:01:00 +0000

    update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -33,4 +33,15 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 
 size_t binary_tree_depth(const binary_tree_t *tree);
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
+void binary_tree_delete(binary_tree_t *tree);
+
+int binary_tree_is_root(const binary_tree_t *node);
+
+
+
+
+
 #endif /* _BINARY_TREES_H_ */
